c3c7972c7b361f8945abc5781ffaa44ee0fb6e5b
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2023-04-21 19:06:24 +0800

    Creating author microservice

---
+++ author-microservice/src/main/java/com/misterdiallo/backend/authormicroservice/AuthorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.misterdiallo.backend.authormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorMicroserviceApplication.class, args);
+    }
+
+}
